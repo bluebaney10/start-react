021a1d62805328f4c61753434e29252fc1c3b0f5
blue_baney <bluebaney10@gmail.com>
2018-12-30 15:38:31 +0700

    add react first time

--- src/App.js
+++ src/App.js
@@ -6,20 +6,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <h2 >
+          <a className="rainbow" href="http://www.bluebaney.com/">#Blue </a>
+          </h2>
+
+
+        </div>
       </div>
     );
   }
